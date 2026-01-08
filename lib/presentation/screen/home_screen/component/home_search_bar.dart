b1495ca18271eb2148fad145db2a17e3f73354c1
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-01-08 14:25:16 +0200

    feat: add search bar component

---
+++ lib/presentation/screen/home_screen/component/home_search_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeSearchBar extends StatelessWidget{
+  const HomeSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 24),
+    child: Row(
+      children: [
+        Expanded(
+            child: SearchBar(
+              hintText: 'Search for fruit salad combos',
+              hintStyle: WidgetStatePropertyAll(
+                TextStyle(
+                  color: Color(0xFF86869E),
+                  fontSize: 14,
+                ),
+              ),
+              leading: SvgPicture.asset(
+                width: 16, height: 16,
+                "assets/icons/ic_search.svg",
+              ),
+              backgroundColor: WidgetStatePropertyAll(Color(0xFFF3F4F9)),
+              elevation: WidgetStatePropertyAll(0.0),
+              shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(16)
+                  )
+              ),
+            )
+        ),
+
+        SizedBox(width: 16,),
+
+        SvgPicture.asset(
+            width: 26, height: 17,
+            "assets/icons/ic_filter_horizontal.svg"
+        )
+      ],
+    ),);
+  }
+
+}
---
+++ lib/presentation/screen/home_screen/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:deliveryapp_flutter/presentation/screen/home_screen/component/home_search_bar.dart';
+import 'package:deliveryapp_flutter/presentation/screen/home_screen/component/home_top_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          HomeTopBar(),
+
+          Padding(
+              padding: EdgeInsetsGeometry.directional(start: 24, end: 90, bottom: 24),
+            child: Text(
+              "Hello Tony, What fruit salad combo do you want today?",
+              style: TextStyle(
+                color: Color(0xFF27214D),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+
+          HomeSearchBar()
+
+        ],
+      ),
+    );
+  }
+
+}
