bbfc90010e4146a84a5e524153d53b8953180a32
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-01-10 12:38:20 +0200

    feat: add meal card component

---
+++ lib/presentation/screen/details_screen/component/MealCard.dart
@@ -0,0 +1,31 @@
+import 'package:deliveryapp_flutter/domain/entity/Meal.dart';
+import 'package:deliveryapp_flutter/presentation/components/BackButton.dart';
+import 'package:flutter/cupertino.dart';
+
+class MealCard extends StatelessWidget {
+  final Meal meal;
+
+  const MealCard({super.key, required this.meal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(color: Color(0xFFFFA451)),
+      child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.directional(top: 20, start: 24),
+            child: SizedBox(width: 90, child: BackButton(onClick: () => {}),),
+          ),
+
+          Padding(
+            padding: EdgeInsetsGeometry.directional(top: 62, bottom: 32),
+            child: Image.network(meal.imageUrl, width: 176, height: 176),
+          ),
+        ],
+      ),
+    );
+  }
+}
