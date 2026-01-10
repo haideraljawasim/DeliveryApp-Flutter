a95bda065a5e2b1e62d60686b67d932b06daa066
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2026-01-10 13:41:32 +0200

    feat: add DetailsScreen route

--- lib/presentation/navigation/GoRouter.dart
+++ lib/presentation/navigation/GoRouter.dart
@@ -1,3 +1,4 @@
+import 'package:deliveryapp_flutter/presentation/screen/details_screen/DetailsScreen.dart';
 import 'package:deliveryapp_flutter/presentation/screen/home_screen/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -25,6 +26,12 @@ final GoRouter appRouter = GoRouter(
         return const DeliveryStatusScreen();
       },
     ),
+    GoRoute(
+      path: AppRouts.mealDetails,
+      builder: (BuildContext context, GoRouterState state) {
+        return const DetailsScreen();
+      },
+    ),
   ],
 );
 
@@ -32,4 +39,5 @@ class AppRouts {
   static const String home = '/';
   static const String orderCompleted = '/order_completed';
   static const String deliveryStatus = '/delivery_status';
+  static const String mealDetails = '/meal_details';
 }
